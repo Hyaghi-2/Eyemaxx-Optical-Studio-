38747109bc82b4f4427d3485488aee8ea7ac9512
williamalnodfah <47870450+williamalnodfah@users.noreply.github.com>
2021-04-18 11:35:00 +0300

    online booking module

---
+++ src/app/online-booking-module/online-booking-module.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OnlineBookingModuleRoutingModule } from './online-booking-module-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OnlineBookingModuleRoutingModule
+  ]
+})
+export class OnlineBookingModuleModule { }

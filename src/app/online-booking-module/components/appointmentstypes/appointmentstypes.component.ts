5ac292d95b5d9f795ebf5608d1e235f4ece436f5
h.yaghi <h.yaghi@itsnerd.com>
2021-04-29 22:40:03 +0300

    get Appointment Types step 3

--- src/app/online-booking-module/components/appointmentstypes/appointmentstypes.component.ts
+++ src/app/online-booking-module/components/appointmentstypes/appointmentstypes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AppointmentType } from '../../models/get-stores-types-doctors/appointment-type';
+import { DoctorStoreTypeResponse } from '../../models/get-stores-types-doctors/doctor-store-type-response';
+import { BookingModuleService } from '../../services/booking-module-service.service';
 
 @Component({
   selector: 'app-appointmentstypes',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentstypesComponent implements OnInit {
 
-  constructor() { }
+  DoctorStoreTypeData: DoctorStoreTypeResponse = new DoctorStoreTypeResponse();
+  accountsId: number = 2040;
+  companyName: string = 'test EyeMaxx';
+  constructor(private serv: BookingModuleService) { }
 
   ngOnInit(): void {
+    this.serv.getStoresTypesDoctors(this.accountsId, this.companyName).subscribe(t => {
+      this.DoctorStoreTypeData.Initialize(t);
+      console.log(this.DoctorStoreTypeData);
+
+    });
+
   }
 
 }

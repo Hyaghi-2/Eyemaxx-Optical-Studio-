7f5a34cd8349d5d0557f627373cba027a5aae594
williamalnodfah <47870450+williamalnodfah@users.noreply.github.com>
2021-04-29 12:59:08 +0300

    steps management initial

--- src/app/online-booking-module/components/base-content/base-content.component.ts
+++ src/app/online-booking-module/components/base-content/base-content.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LoginBody } from '../../models/login/login-body';
 import { LoginResponse } from '../../models/login/login-response';
 import { Step } from '../../models/Step/step';
@@ -10,24 +11,32 @@ import { StepsManagementService } from '../../services/steps-management.service'
   templateUrl: './base-content.component.html',
   styleUrls: ['./base-content.component.css']
 })
-export class BaseContentComponent implements OnInit {
+export class BaseContentComponent implements OnInit ,AfterViewInit{
   loginResponse: LoginResponse = new LoginResponse();
   loginBody: LoginBody = new LoginBody();
-  constructor(private serv: BookingModuleService, private steps: StepsManagementService) { }
+  constructor(private serv: BookingModuleService, private steps: StepsManagementService,
+    private router: Router, private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
-    this.steps.Steps.push(new Step(1, 'COVID19preScr', false, true, false));
-    this.steps.Steps.push(new Step(2, 'ServiceType', false, true, false));
-    this.steps.Steps.push(new Step(3, 'ExamType', false, false, false));
-    this.steps.Steps.push(new Step(4, 'AppointmentsSlots', false, false, false));
-    this.steps.Steps.push(new Step(5, 'OpticianAppointment', false, false, false));
-    this.steps.Steps.push(new Step(6, 'AppointmentConfirmation', false, false, false));
-    this.steps.Steps.push(new Step(7, 'Summary', false, false, false));
-    this.steps.currentStep = this.steps.Steps.filter(x => x.order = 1)[0];
+  ngAfterViewInit(): void {
   }
 
-  stepsEnabled(_order: number): boolean {
-    return this.steps.Steps.filter(x => x.order = _order)[0].enabled;
+  ngOnInit(): void {
+
+  }
+
+  stepsEnabled(_order: number) {
+    let enabled: boolean = this.steps.Steps.filter(x => x.order == _order)[0].enabled;
+    return enabled;
+  }
+
+  currentStepStatus(): Step {
+    return this.steps.currentStep;
+  }
+
+  navigate(id: number) {
+    let s: string = this.steps.Steps.filter(x => x.order == id)[0].route;
+    this.router.navigate([s], { relativeTo: this.route });
   }
 
 }
+/*select*from!groupby@*/
